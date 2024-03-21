22c2645c2b8629a5bd4f073064628c961af64ae3
Artem Shirokikh <artem.shirokikh@gmail.com>
2024-03-21 19:50:46 +0100

    Add entry point

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+
+return array(
+    'name' => 'Guide System',
+    'version' => '0.0.1',
+
+    'persistence' => [
+        'class' => '', // todo
+        'connection' => 'serialized.data',
+
+        'guides' => [
+            // todo
+        ],
+    ],
+);
